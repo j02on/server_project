3e2437dbc4b5be52f771124860e9b927997da7a9
j02on <pjylove08@naver.com>
2026-01-20 09:57:20 +0900

    feat : jsdoc을 이용한 문서화

--- src/routes/TodoRoutes.js
+++ src/routes/TodoRoutes.js
@@ -1,10 +1,47 @@
+/**
+ * @file Todo 관련 라우트 정의
+ * @description Todo CRUD 작업을 위한 REST API 엔드포인트
+ */
+
 const express = require('express');
-const { createTodo, viewTodo, editTodo, delTodo } = require('../controllers/TodoController');
+const { createTodo, viewTodo, editTodo, delTodo, delAllTodo } = require('../controllers/TodoController');
 const router = express.Router();
 
+/**
+ * @route POST /todo
+ * @description 새로운 Todo 생성
+ * @access Public
+ */
 router.post('/', createTodo);
-router.get('/:id', viewTodo);
+
+/**
+ * @route GET /todo
+ * @description 전체 Todo 목록 조회
+ * @access Public
+ */
+router.get('/', viewTodo);
+
+/**
+ * @route PATCH /todo/:id
+ * @description 특정 Todo 수정
+ * @param {number} id - Todo ID
+ * @access Public
+ */
 router.patch('/:id', editTodo);
+
+/**
+ * @route DELETE /todo/:id
+ * @description 특정 Todo 삭제
+ * @param {number} id - Todo ID
+ * @access Public
+ */
 router.delete('/:id', delTodo);
+
+/**
+ * @route DELETE /todo
+ * @description 전체 Todo 삭제
+ * @access Public
+ */
+router.delete('/', delAllTodo);
 
 module.exports = router;
